106b06d8ef9e9edf33686a0a3f5fec691dbd1113
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2025-09-27 13:59:26 +0530

    Chore: setup frontend repo with shadcn UI and react-router

---
+++ apps/frontend/src/components/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1>Homescreen</h1>
+  )
+}
+
+export default Home
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import './index.css'
-import App from './App.tsx'
+import Home from "./components/Home";
+import App from './App'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/home",
+    element: <Home />
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
